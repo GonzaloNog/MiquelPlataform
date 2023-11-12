f5f6b889de00ab0cee12c24dbe8b4fc2f1392531
Miquel Muntaner <m.muntaner.e@gmail.com>
2023-11-12 14:29:43 +0100

    Animación y cámara

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class CharacterControler : MonoBehaviour
 {
-    [Header("Player Skils")]
+    [Header("Player Skills")]
     public float moveSpeed = 1f;
     public float jumpForce = 5f;
+
     [Header("Layer")]
     public Transform groundCheck;
     public LayerMask groundLayer;
-    [Header("Fisicas")]
+
+    [Header("Phyisics")]
     private Rigidbody2D rb;
     private bool isGrounded;
+
+    float dirX;
+    Animator anim;
+    bool isDead;
     SpriteRenderer spriteRenderer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = rb.GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -28,18 +35,41 @@ public class CharacterControler : MonoBehaviour
 
         Vector2 movement = new Vector2(horizontalInput, 0).normalized;
 
+        moveSpeed = Input.GetKey(KeyCode.LeftShift) ? 10f : 5f;
         rb.velocity = new Vector2(movement.x * moveSpeed,rb.velocity.y);
 
-        Debug.Log(isGrounded);
         if(isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("jump");
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
 
-        if (movement.x < 0)
-            spriteRenderer.flipX = true;
-        else 
-            spriteRenderer.flipX = false;
+        SetAnimationState();
+
+        if (!isDead)
+        {
+            dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+            if (dirX < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (dirX > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+        }
+    }
+
+    void SetAnimationState()
+    { 
+        anim.SetBool("isRunning", Input.GetKey(KeyCode.LeftShift) && Mathf.Abs(dirX) > 0);
+        anim.SetBool("isWalking", !anim.GetBool("isRunning") && Mathf.Abs(dirX) > 0);
+        anim.SetBool("isJumping", !isGrounded);
+
     }
 }
